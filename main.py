027b0ba3e290245747ed1e960bc1721134e1cb57
Martines1 <slovak.m10@gmail.com>
2024-03-20 16:42:38 +0100

    Modified loop, added player texture

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame as py
 import math
 from pozadie import Pozadie
 from structure_creator import StructureCreator
-
+import player as pl
 #miesto pred samotny beh kodu, nie triedy atd
 
 
@@ -30,20 +30,31 @@ py.init()
 
 timer = py.time.Clock()
 running = True
+
+#nacitaj hraca
+player = pl.Player()
+all_sprites = py.sprite.Group()
+all_sprites.add(player)
+
+game_speed = 2
 while(running):
     timer.tick(pozadie.fps)
-
+    pressed_key = py.key.get_pressed()
     #vykreslenie pozadia
     for i in range(0,tiles):
+
         pozadie.obrazovka.blit(pozadie.background,[i*bg_width+scroll_bg,0])
     scroll_bg-=2
     if(abs(scroll_bg)>bg_width):
         scroll_bg = 0
-
+    for entity in all_sprites:
+        pozadie.obrazovka.blit(entity.curr_image, entity.rect)
     #vykreslenie prekazok
     base_x = 1200
     base_y = 661
-
+    player.rect.move_ip(-game_speed, 0)
+    if player.rect.x < 10:
+        py.quit()
     if phase==0:#les
         for ob in obstacles_0:
             if(ob=="nic"):
@@ -52,8 +63,8 @@ while(running):
                 move_by = structure_creator.draw(pozadie,ob,base_x+scroll_p,base_y)
                 base_x+=move_by
 
-    scroll_p-=2
-
+    scroll_p-=1
+    player.update(pressed_key)
 
     for event in py.event.get():
         if(event.type==py.QUIT):
