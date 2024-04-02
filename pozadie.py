0ebcdd62d8e0fb645207318431e26118694acfbd
jakino22 <146848411+jakino22@users.noreply.github.com>
2024-04-02 22:13:05 +0200

    Add files via upload

--- pozadie.py
+++ pozadie.py
@@ -4,7 +4,10 @@ class Pozadie:
     def __init__(self, WIDTH, HEIGHT):
         self.obrazovka = py.display.set_mode([WIDTH,HEIGHT])
         py.display.set_caption('SNH')
-        self.background = py.image.load("pozadie.png").convert()
+        self.fixed_background = py.image.load("images/pozadie_tatry.png").convert()
+        self.background = py.image.load("images/popredie_les.png").convert()
+        self.background.set_colorkey((0, 0, 0))
+        py.Surface.convert_alpha(self.background)
         
-    def change_phase(self, phase, py):
-        self.background = py.image.load("pozadie" + phase + ".png").convert()
+    def change_phase(self, postfix, py):
+        self.background = py.image.load("images/popredie_" + postfix + ".png").convert()
